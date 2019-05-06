2f9f8f65d6ad32a28b858018a76fa9f2cb12fb61
Md Shariful Alam <shariful.alam.shourov@gmail.com>
2019-05-06 22:27:32 +0300

    Add files via upload

---
+++ Temperature-converter/Temp.js
@@ -0,0 +1,48 @@
+onChange('from');
+			onChange('to');
+			function onChange(fromOrTo) {
+				if(fromOrTo === 'from') {
+					document.getElementById("from").placeholder = document.getElementById("selectFrom").value;
+				} else if(fromOrTo === 'to') {
+					document.getElementById("to").placeholder = document.getElementById("selectTo").value;
+				} 
+			}
+			function calculate() {
+				var from = document.getElementById("selectFrom");
+				var to = document.getElementById("selectTo");
+				var fromValue = parseInt(document.getElementById("from").value);
+				var toValue = parseInt(document.getElementById("to").value);
+				if(from.value == "Celcius") {
+					if(to.value == "Farenheit") {
+						result = ((fromValue * 9)/5) + 32;
+					}
+					else if(to.value == "Kelvin") {
+						result = fromValue + 273;
+					} else {
+						result = "Cannot calculate!";
+					}
+					document.getElementById("to").value = result.toFixed(2);
+				}
+				else if(from.value == "Farenheit") {
+					if(to.value == "Celcius") {
+						result = ((fromValue - 32) * 5)/9
+					}
+					else if(to.value == "Kelvin") {
+						result = ((fromValue + 459.67) * 5)/9;
+					} else {
+						result = "Cannot calculate!";
+					}
+					document.getElementById("to").value = result.toFixed(2);
+				}
+				else if(from.value == "Kelvin") {
+					if(to.value == "Celcius") {
+						result = fromValue - 273;
+					}
+					else if(to.value == "Farenheit") {
+						result = 1.8 * (fromValue-273) + 32;
+					} else {
+						result = "Cannot calculate!";
+					}
+					document.getElementById("to").value = result.toFixed(2);
+				}
+			}
